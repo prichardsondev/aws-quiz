3e5afd5b5c40ae0804612d8468eceb237a30c83f
prichardsondev <prichardsondev@gmail.com>
2021-06-07 13:11:02 -0400

    updated /backend/public/index.html

--- backend/service.js
+++ backend/service.js
@@ -6,10 +6,12 @@ const { db } = require("./db");
 const service = {
     putQuestion: async (body,catagory) => {
         try {
+            //reshape data
             db.post(body,catagory);
         } catch (err) { }
     },
     getQuestions: async (catagory) => {
+        //reshape data
         let questions = await db.get(catagory);
         return questions;
     },
